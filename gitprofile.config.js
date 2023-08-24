52d17e33cb2bfcc4815103b715bc090bf6c64e3f
Roger <rogersinghchugh@gmail.com>
2023-08-24 13:29:52 -0400

    seventh commit

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'RogerSinghChugh', // Your GitHub org/user name. (Required)
+    username: 'rogersinghchugh', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
